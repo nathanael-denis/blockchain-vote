edca078b94a8f97f636d397b9be210fb2898ea39
Nathanaël Denis <nathanael.denis@telecom-sudparis.eu>
2019-04-16 19:36:19 +0200

    Codage de la fonction appelée pour voter

--- blockchain-based-voting-system/lib/logic.js
+++ blockchain-based-voting-system/lib/logic.js
@@ -9,7 +9,7 @@ const NS='org.vote';
 *@param {org.vote.modifierDocument} maj - La mise a jour du document
 *@transaction
 */
-async function modifierDocument(maj){
+async function modifierDocument(maj){ // vérifier async pour cette fonction
 
   //Get asset registry for Document
   var firstTimeStamp= new Date().getTime;
@@ -59,11 +59,56 @@ async function modifierDocument(maj){
 }
 
 /**
-*Rajoute la modification à la version précédente du texte
-*@param {org.vote.demarrerVotes} maj - La mise a jour du document
+*Lance le protocole de vote après la demande de l'utilisateur
+*@param {org.vote.demarrerVotes} vote - Le vote
 *@transaction
 */
 
-async function demarrerVotes(maj){
-  fo
+async function demarrerVotes(vote){
+  const documentRegistry = await getAssetRegistry(NS + '.Document');
+  const document = await documentRegistry.get(vote.document.getIdentifier());
+  //On s'assure que le document existe
+  if(!document){
+    throw new Error('Le document avec l\'Id $document.getIdentifier()} n\'existe pas');
+  }
+  // On s'assure que l'utilisateur a les droits
+  var nbOui=0; // nombre de oui
+  var nbNon=0; // nombre de non
+  /* On traite les différentes réponses des clients */
+  for(var i=0;n = vote.listeVotants.length; i++){
+  var correct=false; // la réponse est-elle correcte?
+  while(!correct){
+  var choix = prompt("Validez (OUI) ou refusez (NON) la modification");
+  if(choix!="OUI" || choix !="NON"){
+    alert("OUI et NON seules réponses valides; veuillez entrer une réponse");
+  }
+  if(choix=="OUI"){
+    correct=true;
+    nbOui++;
+  }
+  if(choix=="NON"){
+    correct=true;
+    nbNon++;
+  }
+}
+}
+let voteTermineEvent = getFactory().newEvent(NS, 'protocoleVote');
+voteTermineEvent.resultat=false; // par défaut pour faciliter les vérifications
+voteTermineEvent.protocoleVote=vote.protocoleVote
+voteTermineEvent.documentId=vote.documentId;
+/* On fait le bilan des résulats des demarrerVotes
+*si le protocole est majorité, on vérifie que nbOui>nbNon
+* sinon que nbNon est égal à 0
+*/
+if(vote.protocoleVote=="MAJORITE"){
+  if(nbOui<nbNon){
+    voteTermineEvent.resultat=true;
+  }
+}
+if(vote.protocoleVote=="UNANIMITE"){
+  if(nbNon==0){
+    voteTermineEvent.resultat=true;
+  }
+}
+emit(voteTermineEvent);
 }
